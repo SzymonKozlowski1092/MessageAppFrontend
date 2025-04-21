6eb8a0314446936a3623b779e6bc2987ccd7441f
Szymon Kozłowski <szymonkozlowski1092@gmail.com>
2025-04-21 14:59:58 +0200

    Add project files.

---
+++ MessageAppFrontend/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MessageAppFrontend
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
