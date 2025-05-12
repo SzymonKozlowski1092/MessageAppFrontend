7bcfe43f52c65bb8a8308f5c050f417c334a85fd
Szymon Kozlowski <szymonkozlowski1092@gmail.com>
2025-05-12 22:10:46 +0200

    Create ChatInvitation and implement ChatInvitationApiService

---
+++ MessageAppFrontend/Models/ChatInvitation.cs
@@ -0,0 +1,11 @@
+﻿namespace MessageAppFrontend.Models
+{
+    public class ChatInvitation
+    {
+        public Guid Id { get; set; }
+        public Guid ChatId { get; set; }
+        public string ChatName { get; set; } = string.Empty;
+        public string InvitedByUsername { get; set; } = string.Empty;
+        public DateTime SentAt { get; set; }
+    }
+}
---
+++ MessageAppFrontend/Services/Interfaces/IChatInvitationApiService.cs
@@ -0,0 +1,10 @@
+﻿using MessageAppFrontend.Common;
+using MessageAppFrontend.Models;
+
+namespace MessageAppFrontend.Services.Interfaces
+{
+    public interface IChatInvitationApiService
+    {
+        Task<ApiResponse<List<ChatInvitation>>> GetActiveChatInvitations();
+    }
+}
